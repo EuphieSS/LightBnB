f581eb64286b6fc9844016a9e7d90fead2cf5ad0
EuphieSS <samma118@hotmail.com>
2023-02-10 03:58:23 +0000

    Update function to save new properties to database

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -60,14 +60,14 @@ exports.getUserWithId = getUserWithId;
  * @return {Promise<{}>} A promise to the user.
  */
 const addUser =  function(user) {
-  const userInfo = [user.name, user.email, user.password];
+  const queryParams = [user.name, user.email, user.password];
 
   return pool
   .query(`
     INSERT INTO users (name, email, password)
     VALUES ($1, $2, $3)
     RETURNING *;
-    `, userInfo)
+    `, queryParams)
   .then((result) => {
     return result.rows[0];
   })
@@ -88,7 +88,7 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function(guest_id, limit = 10) {
-  const values = [guest_id, limit];
+  const queryParams = [guest_id, limit];
 
   const queryString = `
     SELECT
@@ -105,7 +105,7 @@ const getAllReservations = function(guest_id, limit = 10) {
   `
 
   return pool
-  .query(queryString, values)
+  .query(queryString, queryParams)
   .then((result) => {
     return result.rows;
   })
@@ -127,8 +127,14 @@ exports.getAllReservations = getAllReservations;
  * @return {Promise<[{}]>}  A promise to the properties.
  */
 const getAllProperties = function(options, limit = 10) {
+  //Setup an array to hold any parameters that may be available for the query
   const queryParams = [];
 
+
+  //Start the query with all information that comes before the list of conditions
+  /* WHERE 1=1 is useful when the list of conditions is not known at compile time 
+  and is instead built at run time. With the 1=1, conditions can be generated like 
+  [ and <condition> ], and the initial 'and' has something to associate with */
   let queryString = `
     SELECT properties.*, AVG(property_reviews.rating) AS average_rating
     FROM properties
@@ -136,6 +142,7 @@ const getAllProperties = function(options, limit = 10) {
     WHERE 1=1
   `;
 
+  //Check if a value has been passed in as an option, add it to the params array, and create a condition
   if (options.city) {
     queryParams.push(`%${options.city}%`);
     queryString += `AND city LIKE $${queryParams.length} `;
@@ -165,7 +172,7 @@ const getAllProperties = function(options, limit = 10) {
     queryString += `HAVING AVG(property_reviews.rating) >= $${queryParams.length}`;
   }
 
-  queryParams.push(limit);
+  queryParams.push(limit); //limit will be 20 as it's hardcoded in apiRoutes.js
   queryString += `
   ORDER BY cost_per_night
   LIMIT $${queryParams.length};
@@ -176,12 +183,12 @@ const getAllProperties = function(options, limit = 10) {
   return pool
     .query(queryString, queryParams)
     .then((result) => {
-      // console.log(result.rows);
       return result.rows;
     })
     .catch((err) => {
       console.log(err.message);
     });
+
 };
 exports.getAllProperties = getAllProperties;
 
@@ -191,9 +198,37 @@ exports.getAllProperties = getAllProperties;
  * @return {Promise<{}>} A promise to the property.
  */
 const addProperty = function(property) {
-  const propertyId = Object.keys(properties).length + 1;
-  property.id = propertyId;
-  properties[propertyId] = property;
-  return Promise.resolve(property);
+  const queryParams = [
+    property.owner_id,
+    property.title,
+    property.description,
+    property.thumbnail_photo_url,
+    property.cover_photo_url,
+    property.cost_per_night * 100,
+    property.street,
+    property.city,
+    property.province,
+    property.post_code,
+    property.country,
+    property.parking_spaces,
+    property.number_of_bathrooms,
+    property.number_of_bedrooms
+  ];
+
+  const queryString = `
+  INSERT INTO properties (owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, street, city, province, post_code, country, parking_spaces, number_of_bathrooms, number_of_bedrooms)
+  VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14)
+  RETURNING *;
+  `;
+
+  return pool
+  .query(queryString, queryParams)
+  .then((result) => {
+    return result.rows;
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
+
 }
 exports.addProperty = addProperty;
